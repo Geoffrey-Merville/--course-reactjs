9ff7e5d355d6d33cc21a007fcd6e2a31613bceb2
gmerville-jehann <gmerville@jehann.fr>
2023-11-13 10:08:24 +0100

    improved the list rendering

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,18 +1,20 @@
 //import { Fragment } from "react";
 
 function ListGroup() {
+  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
   return (
     //<Fragment>
-      <>
-        <h1>List</h1>
-        <ul className="list-group">
-        <li className="list-group-item">An item</li>
-          <li className="list-group-item">A second item</li>
-          <li className="list-group-item">A third item</li>
-          <li className="list-group-item">A fourth item</li>
-          <li className="list-group-item">A fifth one</li>
-          </ul>
-      </> // Alternative way to <Fragment>
+    <>
+      <h1>List</h1>
+      <ul className="list-group">
+        {items.map((item) => (
+          <li className="list-group-item" key={item}>
+            {item}
+          </li>
+        ))}
+      </ul>
+    </> // Alternative way to <Fragment>
     //</Fragment>
   );
 }
