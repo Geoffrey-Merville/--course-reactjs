d31c4547880e1397dd35a66f8570c70889990d5d
gmerville-jehann <gmerville@jehann.fr>
2023-11-13 10:14:58 +0100

    improved the list rendering

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,12 +1,18 @@
 //import { Fragment } from "react";
 
 function ListGroup() {
-  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  items = [];
+
+  //const message = items.length === 0 ? <p>No Item Found</p> : null;
+  // Produces the same render as above
+  const message = items.length === 0 && <p>No Item Found</p>;
 
   return (
     //<Fragment>
     <>
       <h1>List</h1>
+      {message /* Possible to use a function instead if needing arguments */}
       <ul className="list-group">
         {items.map((item) => (
           <li className="list-group-item" key={item}>
