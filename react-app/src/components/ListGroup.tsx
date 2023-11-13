8e658e447d1df767bfcb71ede77afffece458fd6
gmerville-jehann <gmerville@jehann.fr>
2023-11-13 10:21:25 +0100

    added handler function

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,12 +1,15 @@
 //import { Fragment } from "react";
+import { MouseEvent } from "react";
 
 function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-  items = [];
 
   //const message = items.length === 0 ? <p>No Item Found</p> : null;
   // Produces the same render as above
   const message = items.length === 0 && <p>No Item Found</p>;
+
+  // Event handler
+  const handleClick = (event: MouseEvent) => console.log(event);
 
   return (
     //<Fragment>
@@ -14,8 +17,8 @@ function ListGroup() {
       <h1>List</h1>
       {message /* Possible to use a function instead if needing arguments */}
       <ul className="list-group">
-        {items.map((item) => (
-          <li className="list-group-item" key={item}>
+        {items.map((item, index) => (
+          <li className="list-group-item" key={item} onClick={handleClick}>
             {item}
           </li>
         ))}
