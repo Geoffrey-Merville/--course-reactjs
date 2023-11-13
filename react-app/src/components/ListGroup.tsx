d35d5676379c8e443bbaf6ce012f11a30c3ca6e3
gmerville-jehann <gmerville@jehann.fr>
2023-11-13 10:27:47 +0100

    added a hook to handle active list items

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,15 +1,17 @@
 //import { Fragment } from "react";
-import { MouseEvent } from "react";
+import { useState } from "react";
 
 function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
 
+  // Hook
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+  // arr[0] -> variable (selectedIndex)
+  // arr[1] -> updater function
+
   //const message = items.length === 0 ? <p>No Item Found</p> : null;
   // Produces the same render as above
   const message = items.length === 0 && <p>No Item Found</p>;
-
-  // Event handler
-  const handleClick = (event: MouseEvent) => console.log(event);
 
   return (
     //<Fragment>
@@ -18,7 +20,17 @@ function ListGroup() {
       {message /* Possible to use a function instead if needing arguments */}
       <ul className="list-group">
         {items.map((item, index) => (
-          <li className="list-group-item" key={item} onClick={handleClick}>
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
             {item}
           </li>
         ))}
